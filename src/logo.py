2dc1d50f960be89028b05e2b0490a75959a110e0
husky <huskyhacks.mk@gmail.com>
2025-02-04 08:21:14 -0500

    better logo

--- src/logo.py
+++ src/logo.py
@@ -1,13 +1,23 @@
-from colorama import *
+from colorama import Fore, Style
 
 
 def print_logo():
-    print(Fore.CYAN + """
-==========================================================
-                     Cazadora
+    title = "Cazadora"
+    subtitle = "Triage Script for Sussy M365 OAuth Apps"
+    author_info = "Matt Kiely | Principal Security Researcher | Huntress"
 
-        Triage Script for Sussy M365 OAuth Apps
+    border_length = 60
 
-Matt Kiely | Principal Security Researcher | Huntress
-==========================================================
+    title_line = title.center(border_length)
+    subtitle_line = subtitle.center(border_length)
+    author_line = author_info.center(border_length)
+    border = "=" * border_length
+
+    print(Fore.CYAN + f"""
+{border}
+{title_line}
+{subtitle_line}
+
+{author_line}
+{border}
 """ + Fore.RESET)
